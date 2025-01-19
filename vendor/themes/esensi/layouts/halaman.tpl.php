8d9aebc9fa6818f4cfff9e6bec8d3ef9fc541aa0
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2025-01-19 16:41:58 +0700

    fix: halaman paddings

---
+++ vendor/themes/esensi/layouts/halaman.tpl.php
@@ -0,0 +1,38 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php $this->load->view($folder_themes . '/commons/meta') ?>
+    <?php $this->load->view($folder_themes . '/commons/source_css') ?>
+    <?php $this->load->view($folder_themes . '/commons/source_js') ?>
+</head>
+
+<body class="font-primary bg-gray-100">
+
+    <?php if ($single_halaman['id']) : ?>
+        <?php $this->load->view($folder_themes . '/commons/loading_screen') ?>
+        <?php $this->load->view($folder_themes . '/commons/header') ?>
+
+
+        <div class="mx-auto lg:px-12 px-3 flex flex-col lg:flex-row my-5 gap-3 lg:gap-5 justify-between text-gray-600">
+            <main class="lg:w-2/3 w-full overflow-hidden space-y-1 bg-white rounded-lg px-4 py-2 lg:py-4 lg:px-5 shadow">
+                <?php $this->load->view($folder_themes . '/partials/halaman'); ?>
+            </main>
+            <div class="lg:w-1/3 w-full">
+                <?php $this->load->view($folder_themes . '/partials/sidebar') ?>
+            </div>
+        </div>
+
+        <?php $this->load->view($folder_themes . '/commons/footer') ?>
+
+    <?php else : ?>
+        <?php $this->load->view($folder_themes . '/commons/404') ?>
+    <?php endif ?>
+
+    <script src="<?= theme_asset("js/script.min.js?" . THEME_VERSION) ?>"></script>
+
+</body>
+
+</html>
