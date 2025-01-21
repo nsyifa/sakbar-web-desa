d0c064c5e76be141b50f2cbf54499be50ac57b68
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2025-01-21 20:49:38 +0700

    clean: cache and log files

---
+++ storage/logs/log-2025-01-21.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-01-21 12:10:02 --> 404 Page Not Found: 
+ERROR - 2025-01-21 12:10:13 --> 404 Page Not Found: 
+ERROR - 2025-01-21 12:11:37 --> 404 Page Not Found: 
+ERROR - 2025-01-21 12:14:26 --> 404 Page Not Found: 
+ERROR - 2025-01-21 19:30:58 --> Severity: error --> Exception: SQLSTATE[22001]: String data, right truncated: 1406 Data too long for column 'lat' at row 1 (Connection: default, SQL: update `config` set `lat` = -7.03500256807484,108.40539600883274, `lng` = 108.40539600883274, `config`.`updated_at` = 2025-01-21 19:30:58 where `id` = 1) C:\laragon\www\OpenSID-2501.0.0\OpenSID-2501.0.0\vendor\illuminate\database\Connection.php 829
